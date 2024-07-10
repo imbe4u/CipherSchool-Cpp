bc1a0c64c2c5b153d45527a158ea22024788ebd9
Anshu Singh <anshuonwork@gmail.com>
2024-07-11 01:15:19 +0530

    first

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>   //iostream is a library which include all important classes and func
+
+using namespace std;  //std is basically a standard library and namespace is used to 
+//organize code into logical groups and prevent naming conflicts; 
+
+int main(){         //function from where code start
+    cout<<"Hello World"<<endl;
+    return 0;
+}
