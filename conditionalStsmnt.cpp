2b3370c1463e028cc2a3acc3d8fae7315e2a7395
Anshu Singh <anshuonwork@gmail.com>
2024-07-17 02:34:35 +0530

    first commit

---
+++ conditionalStsmnt.cpp
@@ -0,0 +1,11 @@
+//the statement either true or false but cant be both at same time.
+//for and if both the statement is true then if part will run eg. a<b and b<c it this both will be true then if will run
+//for or if one of the statement will be true the also if will run
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<(a>b);
+    cout<<(a>b && c>d);  //it will give output as 0 or 1 as t or f
+}
