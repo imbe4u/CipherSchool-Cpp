4e693be425302c6440334f655a30eedd7074d412
Anshu Singh <anshuonwork@gmail.com>
2024-07-20 12:58:21 +0530

    second commit

--- loops1.cpp
+++ loops1.cpp
@@ -2,7 +2,13 @@
 #include<iostream>
 using namespace std;
 int main(){
-    for(int i=100;i>0;i--){
-        cout<<i;
+    // for(int i=100;i>0;i--){
+    //     cout<<i;
+    // }
+
+    int i=1;        //initialization
+    while(i<101){  //statement
+        cout<<i;   
+        i++;     //increment or update
     }
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+ int i=1;        //initialization
+    while(i<101){  //statement
+        cout<<i;   
+        i++;     //increment or update
+    }
