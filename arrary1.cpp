3857e0575bc8d05b5b3602684611f25d89cb5612
Anshu Singh <anshuonwork@gmail.com>
2024-07-21 15:44:06 +0530

    first commit

---
+++ arrary1.cpp
@@ -0,0 +1,8 @@
+//array is data Structured which store similiar type of data 
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d; 
+    cin>>a>>b>>c>>d;  //here we taken input one by one but what if we had to take input in many no. then we have to use array.
+    cout<<a<<b<<c<<d;
+}
