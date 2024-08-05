5e0d115feb046f86faf0c838fdb10798ff4b86fb
Anshu Singh <anshuonwork@gmail.com>
2024-08-06 01:48:38 +0530

    first commit

---
+++ object.cpp
@@ -0,0 +1,3 @@
+//class is a container which store data memebers , functions;
+//Encapsulation: Encapsulation is the bundling of data and methods(functions) into a single unit called a class
+//Inheritance : transfering properites of one class to othe class or from base class to derived class.
