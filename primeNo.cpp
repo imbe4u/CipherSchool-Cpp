dd1fdcacaf599777b9e683543ca5107e290932d9
Anshu Singh <anshuonwork@gmail.com>
2024-08-20 03:08:06 +0530

    second commit

---
+++ primeNo.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n+1];
+    vector<int>arr1(n+1);
+    for(int i=0;i<=n;i++) arr[i]=i;
+
+    for(int i=2;i<=n;i++){
+        if(arr[i]!=0){
+                        for(int j=i+1;j<=n;j++){
+                            if(arr[j]%arr[i]==0) arr[j]=0;
+                    }}
+    
+}
+
+    for(int k=2;k<=n;k++){
+       
+        if(arr[k]!=0){
+         cout<<arr[k]<<" ";
+         arr1.push_back(arr[k]);
+         }
+
+    }
+if(binary_search(arr1.begin(), arr1.end(),17)) {
+   cout << "Element found\n";
+}else{
+    cout<<"Element Not found";
+}
+
+
+}
